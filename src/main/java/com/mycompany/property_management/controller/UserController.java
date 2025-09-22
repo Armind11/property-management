07ffdb5300f08dfd262447144924331f343a2399
Armind11 <elmehdi.hamdouni@gmail.com>
2025-09-22 23:54:44 +0400

    AOP , Implemented Error handling for register a user Method + @valid + constraint in  UserDTO using of spring-boot-starter-validator

--- src/main/java/com/mycompany/property_management/controller/UserController.java
+++ src/main/java/com/mycompany/property_management/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mycompany.property_management.controller;
 
 import com.mycompany.property_management.dto.UserDTO;
 import com.mycompany.property_management.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,14 +23,14 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@RequestBody() UserDTO userDTO) {
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody() UserDTO userDTO) {
 
         userDTO = userService.register(userDTO);
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO) {
         userDTO = userService.login(userDTO.getOwnerEmail(),userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
--- src/main/java/com/mycompany/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property_management/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.property_management.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +15,12 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is Mandatory")
+    @NotEmpty(message = "Owner Email Cannot Be Empty")
+    @Size(min = 1,max = 50, message = "Owner Email Must Be Between 1 to 50 Characters in length")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password Cannot Be Null")
+    @NotEmpty(message = "Password Cannot Be Empty")
     private String password;
 }
